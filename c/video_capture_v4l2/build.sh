bb2cc8d38ed78f9449a47b6ef3dd11cafb92da9f
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2017-07-28 18:28:48 +0000

    [c/video_capture_v4l2] Comments on build script

--- c/video_capture_v4l2/build.sh
+++ c/video_capture_v4l2/build.sh
@@ -1,1 +1,4 @@
-gcc capture.c -ljpeg -DIO_READ -DIO_MMAP -DIO_USERPTR
+#requires libjpeg-dev
+#to view images, install libcaca-dev and caca-utils and use cacaview.
+
+gcc capture.c -ljpeg -DIO_READ -DIO_MMAP -DIO_USERPTR -g
