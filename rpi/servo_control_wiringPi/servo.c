209463755664b12dcd174a16d3fddd614fe271ee
Ewerton Scaboro da Silva <ewertons@github.com>
2017-11-28 23:03:48 -0800

    Save latest updates to servo control using WiringPi

--- rpi/servo_control_wiringPi/servo.c
+++ rpi/servo_control_wiringPi/servo.c
@@ -1,15 +1,14 @@
 // https://learn.sparkfun.com/tutorials/raspberry-gpio/c-wiringpi-example
 
-#include <stdlib.h>    // Used for printf() statements
-#include <stdio.h>    // Used for printf() statements
-#include <wiringPi.h> // Include WiringPi library!
-
-// Pin number declarations. We're using the Broadcom chip pin numbers.
+#include <stdlib.h>
+#include <stdio.h> 
+#include <string.h>
+#include <wiringPi.h> 
 
 int main(void)
 {
-    int pwmPin = 18; // PWM LED - Broadcom pin 18, P1 pin 12
-    int pwmValue = 500; // Defines the dutycycle [0-1024]
+    int pwmPin = 18; // PWM Pin - Broadcom pin 18, P1 pin 12
+    int pwmDutyCycle = 500; // Defines the duty cycle [0-1024]
     int pwmClock = 1920;
     int pwmRange = 200;
     char command[10];
@@ -20,38 +19,42 @@ int main(void)
     pinMode(pwmPin, PWM_OUTPUT); // Set PWM LED as PWM output
     // pwmFrequency in Hz = 19.2e6 Hz / pwmClock / pwmRange.
 
-	while (1)
-	{
-                pwmSetClock(pwmClock);
-                pwmSetRange(pwmRange);
-		pwmWrite(pwmPin, pwmValue); // PWM LED at bright setting
-		printf("\r\nEnter: duty cycle value [0 to 1024] or 'q' to quit: ");
-		
-		if (scanf("%s %d", &command, &value) != 2 || command[0] == 'q')
-		{
-			break;
-		}
-		else if (command[0] == 'c')
-		{
-			pwmClock = value;
-			printf("\r\nClock value set to %d", pwmClock);
-		}
-		else if (command[0] == 'r')
-		{
-			pwmRange = value;
-			printf("\r\nRange value set to %d", pwmRange);
-		}
-		else if (command[0] == 'd')
-		{
-			pwmValue =value;
-			printf("\r\nDuty cycle value set to %d", pwmValue);
-		}
-		else
-		{
-			break;
-		}
-	}
-	
+    while (1)
+    {
+        pwmSetClock(pwmClock);
+        pwmSetRange(pwmRange);
+        pwmWrite(pwmPin, pwmDutyCycle);
+
+        printf("\r\nClock=%d, Range=%d, DutyCycle=%d (Frequency=%d Hz)", pwmClock, pwmRange, pwmDutyCycle, 19200000 / pwmClock / pwmRange);
+
+        printf("\r\n\r\nEnter command ('c [0-4095]' for clock; 'r [0-4095]' for range; 'd [1-1024]' for duty cycle; 'q' to quit): ");
+       
+        memset(command, 0, sizeof(command));
+        (void)scanf("%s %d", &command, &value);
+ 
+        if (command[0] == 'q')
+        {
+            break;
+        }
+        else if (command[0] == 'c')
+        {
+            pwmClock = value;
+        }
+        else if (command[0] == 'r')
+        {
+            pwmRange = value;
+        }
+        else if (command[0] == 'd')
+        {
+            pwmDutyCycle =value;
+        }
+        else
+        {
+            printf("Command '%s' not recognized", command);
+            break;
+        }
+    }
+    
     return 0;
 }
 
