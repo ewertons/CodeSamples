be4b472ec39eaacce62f6267f48bbd02049a2f38
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2018-05-31 14:40:33 -0700

    C# sample for compressing files

---
+++ csharp/FileCompression/FileCompression/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace FileCompression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes("abcdefghijklmnopqrstuvwxyz0123456789");
+
+            const string filename = ".\\sample.gz";
+
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+
+            Stream stream = new FileStream(filename, FileMode.CreateNew);
+            GZipStream gzipStream = new GZipStream(stream, CompressionLevel.Optimal);
+
+            int totalsize;
+            for (totalsize = 0; totalsize < 1000000; totalsize += buffer.Length)
+            {
+                gzipStream.Write(buffer, 0, buffer.Length);
+            }
+
+            gzipStream.Close();
+
+            FileInfo fileInfo = new FileInfo(filename);
+
+            Console.WriteLine($"Original size: {totalsize} bytes; compressed size: {fileInfo.Length} bytes ({100.0  - 100.0 * fileInfo.Length / totalsize }%)");
+        }
+    }
+}
