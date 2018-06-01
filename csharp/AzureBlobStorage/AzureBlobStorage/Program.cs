a802c9bb2f1cbfee158d286db1eb98b94bb15fac
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2018-05-31 23:08:08 -0700

    C# sample for using Azure Blob Storage API

---
+++ csharp/AzureBlobStorage/AzureBlobStorage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureBlobStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reference: https://stackoverflow.com/questions/40109286/accessing-azure-file-storage-from-azure-function
+
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=MYACCOUNT;AccountKey=MYKEY;EndpointSuffix=core.windows.net";
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("myblob");
+
+            byte[] content = Encoding.UTF8.GetBytes("bla bla bla blllllaaaaaaaa!!!");
+            MemoryStream ms = new MemoryStream();
+            ms.Write(content, 0, content.Length);
+            ms.Seek(0, SeekOrigin.Begin);
+            
+            blockBlob.UploadFromStreamAsync(ms).Wait();
+        }
+    }
+}
