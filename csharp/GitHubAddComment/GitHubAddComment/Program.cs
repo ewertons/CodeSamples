54f865b84d124788843a62aa1f4943c415f8c94c
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2018-07-16 22:43:38 -0700

    Example of how to post a comment on a GitHub issue

---
+++ csharp/GitHubAddComment/GitHubAddComment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static async Task RunAsync()
+        {
+            string UserName = "MyGithubUsername";
+            string SecurityToken = "OneOfMyPersonalAccessTokens";
+            string encodedAuth = Convert.ToBase64String(Encoding.UTF8.GetBytes(UserName + ":" + SecurityToken));
+
+            string uri = "https://api.github.com/repos/MyGithubUsername/MyRepo/issues/123/comments";
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, uri);
+            request.Headers.Add("User-Agent", "AutoFeedbackRequester");
+            request.Headers.Add("Authorization", $"Basic ${encodedAuth}");
+
+            request.Content = new ByteArrayContent(Encoding.UTF8.GetBytes("{ \"body\": \"BLABLABLA\" }"));
+            
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(content);
+        }
+
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+    }
+}
