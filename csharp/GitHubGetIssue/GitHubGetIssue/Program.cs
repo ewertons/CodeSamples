4b886a3f798181bac3aee048af3080984acc77b1
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2018-07-17 21:29:42 -0700

    Add more github API samples (get issue, get issue comments)

---
+++ csharp/GitHubGetIssue/GitHubGetIssue/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubGetIssue
+{
+    class Program
+    {
+        private static async Task RunAsync()
+        {
+            string result;
+            string issueUrl = "https://api.github.com/repos/Azure/azure-iot-sdk-c/issues/507";
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string requestUrl = issueUrl;
+
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+                request.Headers.Add("User-Agent", "SampleApp");
+
+                var response = await httpClient.SendAsync(request);
+
+                result = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(result);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+    }
+}
